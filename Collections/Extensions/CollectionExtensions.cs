3989a3fc5750f198b34927f38f5df11674dea837
kamasylvia <zhangyue.dev@outlook.com>
2022-02-28 06:33:17 +0000

    Format source code with CSharpier [skip ci]

--- Collections/Extensions/CollectionExtensions.cs
+++ Collections/Extensions/CollectionExtensions.cs
@@ -5,6 +5,7 @@ public static class CollectionExtensions
     public static string ToDisplayString<T>(this IEnumerable<T> ary) =>
         $"[{string.Join(", ", ary)}]";
 
-    public static IEnumerable<string> ToDisplayStrings<TKey, TValue>(this IDictionary<TKey, TValue> dict) =>
-        dict.Select(element => $"{{\"{element.Key} \": \"{element.Value}\"}}");
+    public static IEnumerable<string> ToDisplayStrings<TKey, TValue>(
+        this IDictionary<TKey, TValue> dict
+    ) => dict.Select(element => $"{{\"{element.Key} \": \"{element.Value}\"}}");
 }
